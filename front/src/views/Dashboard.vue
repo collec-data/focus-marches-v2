46e56533cce78b3a49929286318f9f175083005d
leoa <leo.arurault@numihfrance.fr>
2025-09-15 14:18:17 +0200

    affichage des premiers indicateurs

--- front/src/views/Dashboard.vue
+++ front/src/views/Dashboard.vue
@@ -1,21 +1,13 @@
 <script setup>
-import BestSellingWidget from '@/components/dashboard/BestSellingWidget.vue';
 import BuyersRanking from '@/components/dashboard/BuyersRanking.vue';
 import CategoryDistribution from '@/components/dashboard/CategoryDistribution.vue';
 import KeyIndicators from '@/components/dashboard/KeyIndicators.vue';
-import NotificationsWidget from '@/components/dashboard/NotificationsWidget.vue';
-import RecentSalesWidget from '@/components/dashboard/RecentSalesWidget.vue';
-import RevenueStreamWidget from '@/components/dashboard/RevenueStreamWidget.vue';
-import StatsWidget from '@/components/dashboard/StatsWidget.vue';
 </script>
 
 <template>
+    <KeyIndicators />
 
-      <KeyIndicators />
+    <CategoryDistribution />
 
-
-      <CategoryDistribution />
-
-
-<BuyersRanking />
+    <BuyersRanking />
 </template>
