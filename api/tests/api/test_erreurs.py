f4202c18f59928174b8d3f6adedb7873f42d7542
leoa <leo.arurault@numihfrance.fr>
2025-10-08 16:13:45 +0200

    feat/api: nouvel endpoint de statistique sur les erreurs d'import

--- api/tests/api/test_erreurs.py
+++ api/tests/api/test_erreurs.py
@@ -9,3 +9,42 @@ def test_list_erreurs(client):
     assert response.status_code == 200
     assert len(response.json()) == 2
     assert len(response.json()[0]["erreurs"])
+
+
+def test_get_stats_aucune_erreur(client):
+    response = client.get("/erreurs-import/stats")
+
+    assert response.status_code == 200
+    assert response.json() == []
+
+
+def test_get_stats(client):
+    # La factory DecpMalForme genère des objets avec deux erreurs pour chaque DECP.
+    # On va utiliser la première pour tester l'agrégation sur la localisation
+    # et la seconde pour l'agrégation sur le message d'erreur
+
+    DecpMalFormeFactory.create_batch(
+        5,
+        erreurs__0__localisation="montant",
+        erreurs__1__message="Erreur Une",
+    )
+    DecpMalFormeFactory.create_batch(
+        1,
+        erreurs__0__localisation="montant",
+        erreurs__1__message="Erreur Deux",
+    )
+    DecpMalFormeFactory.create_batch(
+        3,
+        erreurs__0__localisation="durée",
+        erreurs__1__message="Erreur Trois",
+    )
+    response = client.get("/erreurs-import/stats")
+
+    assert response.status_code == 200
+    assert response.json() == [
+        {"erreur": "Lorem ipsum dolor", "localisation": "montant", "nombre": 6},
+        {"erreur": "Erreur Une", "localisation": ".", "nombre": 5},
+        {"erreur": "Erreur Trois", "localisation": ".", "nombre": 3},
+        {"erreur": "Lorem ipsum dolor", "localisation": "durée", "nombre": 3},
+        {"erreur": "Erreur Deux", "localisation": ".", "nombre": 1},
+    ]
