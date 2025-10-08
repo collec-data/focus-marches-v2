9e3f76239026a74913a5688a8b0c42cb9cb43a78
leoa <leo.arurault@numihfrance.fr>
2025-10-08 14:08:51 +0200

    feat/front: fonction de découpage des labels trop longs

--- front/src/service/HelpersService.ts
+++ front/src/service/HelpersService.ts
@@ -13,3 +13,25 @@ export const formatDate = (value: Date) => {
         day: 'numeric'
     });
 };
+
+/**
+ * Insère des retours à la ligne quand les labels sont trop longs
+ * @param labels La liste de labels à modifier
+ * @param length La longueur à partir de laquelle on coupe les labels
+ * @returns La liste modifiée
+ */
+export function longLabelsBreaker(labels: Array<string | null>, length: number = 13): Array<string | null> {
+    function addLineBreak(e: string, length: number): string {
+        if (e && e.length > length) {
+            const index = e.indexOf(' ', length);
+            if (index > 1) {
+                e = e.substring(0, index) + '<br>' + addLineBreak(e.substring(index), length);
+            }
+            return e;
+        }
+        return e;
+    }
+    return labels.map((e) => {
+        return e ? addLineBreak(e, length) : e;
+    });
+}
