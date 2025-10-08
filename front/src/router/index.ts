195f6670876f210855373356d1a4b304147ea0a3
leoa <leo.arurault@numihfrance.fr>
2025-10-08 17:03:43 +0200

    chore/front: suppression d'une page inutilisée

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -56,11 +56,6 @@ const router = createRouter({
             ]
         },
         {
-            path: '/landing',
-            name: 'landing',
-            component: () => import('@/views/pages/Landing.vue')
-        },
-        {
             path: '/pages/notfound',
             name: 'notfound',
             component: () => import('@/views/pages/NotFound.vue')
