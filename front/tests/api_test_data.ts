3f6e80009a9777c3ff223e439382a36710a45688
leoa <leo.arurault@numihfrance.fr>
2025-10-23 11:00:04 +0200

    feat/front: categorisation pour la distribution temporelle

--- front/tests/api_test_data.ts
+++ front/tests/api_test_data.ts
@@ -1,36 +1,53 @@
-export const marche = {
+import {
+    Ccag,
+    type DecpMalFormeDtoOutput,
+    type IndicateursDto,
+    type MarcheAllegeDtoOutput,
+    type MarcheCcagDtoOutput,
+    type MarcheDepartementDtoOutput,
+    type MarcheNatureDtoOutput,
+    type MarcheProcedureDtoOutput,
+    ProcedureMarche,
+    StatsErreursDto,
+    type StructureAggMarchesDtoOutput
+} from '../src/client/types.gen.ts';
+
+export const marche: MarcheAllegeDtoOutput = {
     uid: 1234,
     id: '00001234',
     acheteur: { uid: 42, identifiant: '000000', type_identifiant: 'SIRET', nom: 'Acheteur', vendeur: false, acheteur: true },
     objet: 'Lorem ipsum dolor',
+    categorie: 'Services',
     cpv: '123456',
     sous_traitance_declaree: false,
     actes_sous_traitance: [],
-    date_notification: '2025-01-01',
+    date_notification: new Date('2025-01-01'),
     montant: '1000',
-    titulaires: [{ uid: 4321, identifiant: '00000011111', type_identifiant: 'SIRET', nom: 'Vendeur', vendeur: true, acheteur: false }]
+    titulaires: [{ uid: 4321, identifiant: '00000011111', type_identifiant: 'SIRET', nom: 'Vendeur', vendeur: true, acheteur: false }],
+    considerations_environnementales: [],
+    considerations_sociales: []
 };
 
-export const indicateurs = { periode: null, nb_contrats: 456, montant_total: '10000000000.00', nb_acheteurs: 2, nb_fournisseurs: 3, nb_sous_traitance: 4, nb_innovant: 5 };
+export const indicateurs: IndicateursDto = { periode: null, nb_contrats: 456, montant_total: '10000000000.00', nb_acheteurs: 2, nb_fournisseurs: 3, nb_sous_traitance: 4, nb_innovant: 5 };
 
-export const structure = { structure: { uid: 42, identifiant: '0000000', type_identifiant: 'SIRET', nom: 'Une structure', vendeur: false, acheteur: true }, montant: '10000000', nb_contrats: 500 };
+export const structure: StructureAggMarchesDtoOutput = { structure: { uid: 42, identifiant: '0000000', type_identifiant: 'SIRET', nom: 'Une structure', vendeur: false, acheteur: true }, montant: '10000000', nb_contrats: 500 };
 
-export const nature = [
+export const nature: MarcheNatureDtoOutput[] = [
     { mois: '2025-01', nature: 1, montant: '100', nombre: 2 },
     { mois: '2025-02', nature: 1, montant: '200', nombre: 1 }
 ];
 
-export const procedures = [
-    { procedure: 1, montant: '100.00', nombre: 1 },
-    { procedure: 2, montant: '200.00', nombre: 3 }
+export const procedures: MarcheProcedureDtoOutput[] = [
+    { procedure: ProcedureMarche.APPEL_D_OFFRES_OUVERT, montant: '100.00', nombre: 1 },
+    { procedure: ProcedureMarche.APPEL_D_OFFRES_RESTREINT, montant: '200.00', nombre: 3 }
 ];
 
-export const departements = [
+export const departements: MarcheDepartementDtoOutput[] = [
     { code: '35', montant: '100', nombre: 2 },
     { code: '44', montant: '200', nombre: 1 }
 ];
 
-export const erreurs = [
+export const erreurs: DecpMalFormeDtoOutput[] = [
     {
         uid: 123,
         decp: {},
@@ -38,6 +55,6 @@ export const erreurs = [
     }
 ];
 
-export const erreursStats = [{ erreur: 'Field required', nombre: 1263, localisation: 'offresRecues' }];
+export const erreursStats: StatsErreursDto[] = [{ erreur: 'Field required', nombre: 1263, localisation: 'offresRecues' }];
 
-export const ccag = [{ ccag: 1, montant: '10000', nombre: 123 }];
+export const ccag: MarcheCcagDtoOutput[] = [{ ccag: Ccag.TRAVAUX, montant: '10000', nombre: 123 }];
