6790f6babb1a70845c79ad69ddc89045bf1f3e5a
leoa <leo.arurault@numihfrance.fr>
2025-10-24 12:40:11 +0200

    fix/front: tests timeout

--- front/vite.config.mjs
+++ front/vite.config.mjs
@@ -35,6 +35,7 @@ export default defineConfig({
             reporter: ['cobertura', 'text'],
             provider: 'istanbul',
             reportsDirectory: './coverage'
-        }
+        },
+        testTimeout: 10_000
     }
 });
