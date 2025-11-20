260ef08abb6f0e5a22b81e6238a66b00fce9eabb
leoa <leo.arurault@numihfrance.fr>
2025-11-20 11:16:42 +0100

    feat/front: nom d'opsn et région paramétrables

--- front/src/layout/AppFooter.vue
+++ front/src/layout/AppFooter.vue
@@ -1,11 +1,13 @@
-<script setup></script>
+<script setup>
+import { getOpsnRegion } from '@/service/HelpersService';
+</script>
 
 <template>
     <footer class="layout-footer card">
         <div>
             <h2 class="text-center">A propos de cet outil</h2>
             <div class="columns-2">
-                <p>Cet outil est alimenté avec les données du profil d'acheteur de la salle de marchés de Mégalis Bretagne.</p>
+                <p>Cet outil est alimenté avec les données du profil d'acheteur de la salle de marchés de {{ getOpsnRegion() }}.</p>
                 <p>Il a été conçu par le service Données Ouvertes de ARNia pour faciliter l'exploitation des informations contenues dans les données essentielles de la commande publique.</p>
                 <p>Données publiées sous la Licence Ouverte Version 2.0 - Etalab. Le téléchargement des données se fait librement sur le portail opendata national.</p>
                 <p>Notre page Mentions légales contient des informations concernant la protection de la vie privée et vous permet de configurer le suivi ou pas par notre outil d'analyse de visites.</p>
--- front/src/views/Accueil.vue
+++ front/src/views/Accueil.vue
@@ -6,17 +6,19 @@ import IndicateursCles from '@/components/dashboard/IndicateursCles.vue';
 import NatureContrats from '@/components/dashboard/NatureContrats.vue';
 import Procedure from '@/components/dashboard/Procedure.vue';
 import Top12 from '@/components/dashboard/Top12.vue';
-import { getNow } from '@/service/HelpersService';
+import { getNow, getOpsnRegion } from '@/service/HelpersService';
 
 const dateMin = new Date(settings.date_min);
 const dateMax = getNow();
+const region = settings.region;
+const opsn = settings.opsn;
 </script>
 
 <template>
     <main className="card">
-        <h1>Données essentielles des marchés publics de la plateforme Mégalis Bretagne</h1>
+        <h1>Données essentielles des marchés publics de la plateforme {{ getOpsnRegion() }}</h1>
         <p>
-            Cet outil exploite les données essentielles des marchés publics passés en Bretagne sous réserves que l'organisme qui passe le marché public soit adhérent au service proposé par Mégalis Bretagne et qu'il ait bien complété toutes les
+            Cet outil exploite les données essentielles des marchés publics passés en {{ region }} sous réserves que l'organisme qui passe le marché public soit adhérent au service proposé par {{ opsn }} et qu'il ait bien complété toutes les
             informations nécessaires dans la salle des marchés publics (bien renseigné l'étape décision). On ne trouvera pas, par exemple, les marchés des services de l'Etat qui utilisent leur propre plateforme.
         </p>
         <IndicateursCles :dateMin :dateMax />
