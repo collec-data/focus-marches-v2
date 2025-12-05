bdeb7348770f60a5cefb36382c259046fcfdc964
leoa <leo.arurault@numihfrance.fr>
2025-12-05 12:43:03 +0100

    chore/front: suppression helper inutilisé

--- front/src/service/Departements.ts
+++ front/src/service/Departements.ts
@@ -105,9 +105,3 @@ const listeDepartements = {
 export function getNomDepartement(code: string): string {
     return listeDepartements[code];
 }
-
-export function getDepartementAvecNumeroAsListe() {
-    return Object.entries(listeDepartements).map(([k, v]) => {
-        return { value: `(${k}) ${v}`, code: parseInt(k) };
-    });
-}
